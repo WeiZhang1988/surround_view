caf94e6fd404f0d7001d85179b6c074c099107a2
Wei ZHANG <wei_zhang_1988@outlook.com>
2024-01-23 16:24:45 +0800

    change structure of source file management

--- src/test.cpp
+++ src/test.cpp
@@ -1,13 +1,13 @@
-#include "structures.hpp"
-#include "utils.hpp"
-#include "param_setting.hpp"
-#include "camera_model.hpp"
-#include "imagebuffer.hpp"
-#include "base_thread.hpp"
-#include "capture_thread.hpp"
-#include "process_thread.hpp"
-#include "birdview.hpp"
-#include "simple_gui.hpp"
+#include "../include/structures.hpp"
+#include "../include/utils.hpp"
+#include "../include/param_setting.hpp"
+#include "../include/camera_model.hpp"
+#include "../include/imagebuffer.hpp"
+#include "../include/base_thread.hpp"
+#include "../include/capture_thread.hpp"
+#include "../include/process_thread.hpp"
+#include "../include/birdview.hpp"
+#include "../include/simple_gui.hpp"
 
 namespace SVS{
 
